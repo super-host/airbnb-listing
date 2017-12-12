4dce8fefaee1bca4cd89ee2f1ac23ad33bdbef5f
Vanessa <alfonso@hansacanada.com>
2017-12-11 20:11:25 -0800

    setting up travis

--- test/spec.js
+++ test/spec.js
@@ -1,17 +1,14 @@
 const expect = require('chai').expect;
 require('dotenv').config();
+const request = require('supertest'); //used for testing http
+const app = require('../server/app.js');
 
 describe('Shiftly Backend Test Spec', () => {
   describe('Server Routing:', () => {
     it('should get 200 response with /listings endpoint', (done) => {
-      // request(app)
-      //   .post('/add_employee')
-      //   .set('Content-Type', 'application/json')
-      //   .send({
-      //     username: 'Bob',
-      //     password: 'Bob',
-      //   })
-      //   .expect(200, done);
+      request(app)
+        .get('/listings')
+        .expect(200, done);
     });
   });
 });
